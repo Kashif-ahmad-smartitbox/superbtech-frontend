989eca3c048192911c939188ccfd648b17d74a67
Kashif-ahmad-smartitbox <tech2@smartitbox.in>
2026-01-05 12:00:04 +0530

    Update CarouselSlider.jsx

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react"; 
+import React, { useState, useEffect, useCallback } from "react";  
 import { Link } from "react-router-dom";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { FiPause, FiPlay } from "react-icons/fi";
